985f7f9fa328192efd0f0f0510b8951c0e60df25
hielf <hielf@qq.com>
2015-08-23 23:41:35 +0800

    test

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -1,55 +1,45 @@
 #!/bin/sh
 ### BEGIN INIT INFO
 # Provides:          unicorn
-# Required-Start:    $local_fs $remote_fs
-# Required-Stop:     $local_fs $remote_fs
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: unicorn initscript
-# Description:       unicorn
+# Short-Description: Manage unicorn server
+# Description:       Start, stop, restart unicorn server for a specific application.
 ### END INIT INFO
 set -e
-# Example init script, this can be used with nginx, too,
-# since nginx and unicorn accept the same signals
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/deployer/apps/broker_mgr/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-ENVIRONMENT=production
-PATH="/home/deployer/.rbenv/bin:/home/deployer/.rbenv/shims:$PATH"
-CMD="bin/unicorn -E $ENVIRONMENT -D -c $APP_ROOT/config/unicorn.rb"
-
-cd $APP_ROOT
-if [ "deployer" != `whoami` ]; then
-  CMD="sudo -u deployer -- env PATH=$PATH $CMD"
-fi
-echo $CMD
-
-action="$1"
+CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+AS_USER=deployer
 set -u
 
-old_pid="$PID.oldbin"
-
-cd $APP_ROOT || exit 1
+OLD_PIN="$PID.oldbin"
 
 sig () {
   test -s "$PID" && kill -$1 `cat $PID`
 }
 
 oldsig () {
-  test -s $old_pid && kill -$1 `cat $old_pid`
+  test -s $OLD_PIN && kill -$1 `cat $OLD_PIN`
 }
 
-workersig () {
-  workerpid="$APP_ROOT/tmp/pids/unicorn.$2.pid"
-  test -s "$workerpid" && kill -$1 `cat $workerpid`
+run () {
+  if [ "$(id -un)" = "$AS_USER" ]; then
+    eval $1
+  else
+    su -c "$1" - $AS_USER
+  fi
 }
 
-case $action in
+case "$1" in
 start)
   sig 0 && echo >&2 "Already running" && exit 0
-  $CMD
+  run "$CMD"
   ;;
 stop)
   sig QUIT && exit 0
@@ -62,35 +52,28 @@ force-stop)
 restart|reload)
   sig HUP && echo reloaded OK && exit 0
   echo >&2 "Couldn't reload, starting '$CMD' instead"
-  $CMD
+  run "$CMD"
   ;;
 upgrade)
-  if sig USR2 && sleep 20 && sig 0 && oldsig QUIT
+  if sig USR2 && sleep 2 && sig 0 && oldsig QUIT
   then
     n=$TIMEOUT
-    while test -s $old_pid && test $n -ge 0
+    while test -s $OLD_PIN && test $n -ge 0
     do
       printf '.' && sleep 1 && n=$(( $n - 1 ))
     done
     echo
 
-    if test $n -lt 0 && test -s $old_pid
+    if test $n -lt 0 && test -s $OLD_PIN
     then
-      echo >&2 "$old_pid still exists after $TIMEOUT seconds"
+      echo >&2 "$OLD_PIN still exists after $TIMEOUT seconds"
       exit 1
     fi
-    # add this line to bring up another worker for the scheduler right after the upgrade is done:
-    sig TTIN
     exit 0
   fi
   echo >&2 "Couldn't upgrade, starting '$CMD' instead"
-  $CMD
+  run "$CMD"
   ;;
-kill_worker)
-  workersig QUIT $2 && exit 0
-  echo >&2 "Worker not running"
-  ;;
-
 reopen-logs)
   sig USR1
   ;;
