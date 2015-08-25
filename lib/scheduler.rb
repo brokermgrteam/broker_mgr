796accaf7c8a21e8c905d6199ebc4971656f99df
hielf <hielf@qq.com>
2015-08-25 13:38:01 +0800

    commit

--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -66,13 +66,6 @@ class Scheduler
   # Job-Definitions go here
   #
   def setup_jobs
-    @rufus_scheduler.every '1m', :first_at => Time.now + 10 do
-      Rails.logger.task.info "job start"
-    end
-    @rufus_scheduler.every '2m', :first_at => Time.now + 10 do
-      Rails.logger.task.info "job 2 start"
-    end
-    @rufus_scheduler.join
     # @rufus_scheduler.every '12h' do
     #   @branches = Branch.all
     #   @departments = Department.all
@@ -192,63 +185,64 @@ class Scheduler
     #   Rails.logger.task.info "job syncPositions, ok. #{Time.now}"
     # end
 
-    # @rufus_scheduler.every '12h', :first_at => Time.now + 5 * 60 do
-    #   @branches = Branch.all
-    #
-    #   @branches.each do |br|
-    #     @brokers = br.brokers.where('broker_type = ? and broker_status <> ? and broker_name not like ?', 4119, 407, "离职%")
-    #     if @brokers.count > 0
-    #       api = APP_CONFIG['21tb_api']
-    #       uri = APP_CONFIG['21tb_uri_syncUsers']
-    #       current_api = api + uri + ".html"
-    #       appkey = APP_CONFIG['21tb_appkey']
-    #       secrect = APP_CONFIG['21tb_secrect']
-    #       corpcode = APP_CONFIG['21tb_corpcode']
-    #       signText = secrect + "|" + uri + "|" + secrect
-    #       sign = Digest::MD5.hexdigest(signText).upcase
-    #       timeStamp = DateTime.now.strftime('%Q')
-    #
-    #       l = []
-    #       @brokers.each do |b|
-    #         h = Hash.new
-    #         h[:corpCode] = corpcode
-    #         h[:userName] = b.broker_name.strip
-    #         h[:employeeCode] = b.broker_code
-    #         h[:loginName] = b.broker_code
-    #         h[:organizeCode] = b.branch.code || '0000'
-    #         h[:accountStatus] = (Dict.find(b.broker_status).code == 2) ? 'FORBIDDEN' : 'ENABLE'
-    #         h[:positionCode] = Dict.find(b.broker_level).code.to_s if b.broker_level
-    #         h[:email] = b.email if b.email
-    #         h[:idCard] = b.certificate_num if b.certificate_num
-    #         l << h
-    #       end
-    #
-    #       a = { 'appKey_' => appkey,
-    #             'sign_' => sign,
-    #             'timestamp_' => timeStamp,
-    #             'users' => l.to_json,
-    #             'updatePswFirstLogin' => true}
-    #
-    #       http_client = Net::HTTP.new(URI.parse(current_api).host, URI.parse(current_api).port)
-    #
-    #       timeout = 600
-    #       http_client.read_timeout = timeout
-    #
-    #       begin
-    #         Timeout::timeout(timeout) do
-    #           http_client.start do |http|
-    #             req = Net::HTTP::Post.new(URI.parse(current_api))
-    #             req.form_data = a
-    #             resp = http.request(req)
-    #           end
-    #         end
-    #         ensure
-    #           http_client.finish rescue nil
-    #       end
-    #     end
-    #   end
-    #   Rails.logger.task.info "job syncUsers, ok. #{Time.now}"
-    # end
+    @rufus_scheduler.every '12h', :first_at => Time.now + 5 * 60 do
+      Rails.logger.task.info "job syncUsers start"
+      @branches = Branch.all
+
+      @branches.each do |br|
+        @brokers = br.brokers.where('broker_type = ? and broker_status <> ? and broker_name not like ?', 4119, 407, "离职%")
+        if @brokers.count > 0
+          api = APP_CONFIG['21tb_api']
+          uri = APP_CONFIG['21tb_uri_syncUsers']
+          current_api = api + uri + ".html"
+          appkey = APP_CONFIG['21tb_appkey']
+          secrect = APP_CONFIG['21tb_secrect']
+          corpcode = APP_CONFIG['21tb_corpcode']
+          signText = secrect + "|" + uri + "|" + secrect
+          sign = Digest::MD5.hexdigest(signText).upcase
+          timeStamp = DateTime.now.strftime('%Q')
+
+          l = []
+          @brokers.each do |b|
+            h = Hash.new
+            h[:corpCode] = corpcode
+            h[:userName] = b.broker_name.strip
+            h[:employeeCode] = b.broker_code
+            h[:loginName] = b.broker_code
+            h[:organizeCode] = b.branch.code || '0000'
+            h[:accountStatus] = (Dict.find(b.broker_status).code == 2) ? 'FORBIDDEN' : 'ENABLE'
+            h[:positionCode] = Dict.find(b.broker_level).code.to_s if b.broker_level
+            h[:email] = b.email if b.email
+            h[:idCard] = b.certificate_num if b.certificate_num
+            l << h
+          end
+
+          a = { 'appKey_' => appkey,
+                'sign_' => sign,
+                'timestamp_' => timeStamp,
+                'users' => l.to_json,
+                'updatePswFirstLogin' => true}
+
+          http_client = Net::HTTP.new(URI.parse(current_api).host, URI.parse(current_api).port)
+
+          timeout = 600
+          http_client.read_timeout = timeout
+
+          begin
+            Timeout::timeout(timeout) do
+              http_client.start do |http|
+                req = Net::HTTP::Post.new(URI.parse(current_api))
+                req.form_data = a
+                resp = http.request(req)
+              end
+            end
+            ensure
+              http_client.finish rescue nil
+          end
+        end
+      end
+      Rails.logger.task.info "job syncUsers, ok. #{Time.now}"
+    end
 
     # @rufus_scheduler.every '12h' do
     #   api = APP_CONFIG['21tb_api']
@@ -365,6 +359,7 @@ class Scheduler
     #   end
     #   Rails.logger.task.info "job getExams, ok. #{Time.now}"
     # end
+    @rufus_scheduler.join
     Rails.logger.task.info "job done, ok. #{Time.now}"
   end
 
