04a7efa65cb3c3e78a155ec37098e51b5a322a0d
hielf <hielf@qq.com>
2015-09-13 23:12:08 +0800

    commit

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,60 +1,61 @@
 class Schedule < ActiveRecord::Base
   # attr_accessible :title, :body
   def self.task
-    Rails.logger.task.info "job sync21tbOrganizes, start ok. #{Time.now}"
+    Rails.logger.task.info "job syncUsers, start ok. #{Time.now}"
     @branches = Branch.all
-    @departments = Department.all
-    api = APP_CONFIG['21tb_api']
-    uri = APP_CONFIG['21tb_uri_syncOrganizes']
-    current_api = api + uri + ".html"
-    appkey = APP_CONFIG['21tb_appkey']
-    secrect = APP_CONFIG['21tb_secrect']
-    corpcode = APP_CONFIG['21tb_corpcode']
-    signText = secrect + "|" + uri + "|" + secrect
-    sign = Digest::MD5.hexdigest(signText).upcase
-    timeStamp = DateTime.now.strftime('%Q')
 
-    l = []
-    @departments.each do |d|
-      h = Hash.new
-      h[:corpCode] = corpcode
-      h[:organizeCode] = d.code
-      h[:organizeName] = d.name.strip
-      h[:parentCode] = "*"
-      l << h
-    end
-    @branches.each do |b|
-      h = Hash.new
-      h[:corpCode] = corpcode
-      h[:organizeCode] = b.code
-      h[:organizeName] = b.name.strip
-      h[:parentCode] = b.department.code
-      l << h
-    end
+    @branches.each do |br|
+      @brokers = br.brokers.where('broker_type = ? and broker_status <> ? and broker_name not like ?', 4119, 407, "离职%")
+      if @brokers.count > 0
+        api = APP_CONFIG['21tb_api']
+        uri = APP_CONFIG['21tb_uri_syncUsers']
+        current_api = api + uri + ".html"
+        appkey = APP_CONFIG['21tb_appkey']
+        secrect = APP_CONFIG['21tb_secrect']
+        corpcode = APP_CONFIG['21tb_corpcode']
+        signText = secrect + "|" + uri + "|" + secrect
+        sign = Digest::MD5.hexdigest(signText).upcase
+        timeStamp = DateTime.now.strftime('%Q')
 
-    a = { 'appKey_' => appkey,
-          'sign_' => sign,
-          'timestamp_' => timeStamp,
-          'organizes' => l.to_json }
+        l = []
+        @brokers.each do |b|
+          h = Hash.new
+          h[:corpCode] = corpcode
+          h[:userName] = b.broker_name.strip
+          h[:employeeCode] = b.broker_code
+          h[:loginName] = b.broker_code
+          h[:organizeCode] = b.branch.code || '0000'
+          h[:accountStatus] = (Dict.find(b.broker_status).code == 2) ? 'FORBIDDEN' : 'ENABLE'
+          h[:positionCode] = Dict.find(b.broker_level).code.to_s if b.broker_level
+          h[:email] = b.email if b.email
+          h[:idCard] = b.certificate_num if b.certificate_num
+          l << h
+        end
 
-    http_client = Net::HTTP.new(URI.parse(current_api).host, URI.parse(current_api).port)
-    # http_client.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    # http_client.use_ssl = true
+        a = { 'appKey_' => appkey,
+              'sign_' => sign,
+              'timestamp_' => timeStamp,
+              'users' => l.to_json,
+              'updatePswFirstLogin' => true}
 
-    timeout = 240
-    http_client.read_timeout = timeout
+        http_client = Net::HTTP.new(URI.parse(current_api).host, URI.parse(current_api).port)
 
-    begin
-      Timeout::timeout(timeout) do
-        http_client.start do |http|
-          req = Net::HTTP::Post.new(URI.parse(current_api))
-          req.form_data = a
-          resp = http.request(req)
+        timeout = 600
+        http_client.read_timeout = timeout
+
+        begin
+          Timeout::timeout(timeout) do
+            http_client.start do |http|
+              req = Net::HTTP::Post.new(URI.parse(current_api))
+              req.form_data = a
+              resp = http.request(req)
+            end
+          end
+          ensure
+            http_client.finish rescue nil
         end
       end
-      ensure
-        http_client.finish rescue nil
     end
-    Rails.logger.task.info "job sync21tbOrganizes, ok. #{Time.now}"
+    Rails.logger.task.info "job syncUsers, ok. #{Time.now}"
   end
 end
