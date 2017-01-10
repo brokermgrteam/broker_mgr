068849ace6d8fdd87f814b480c3bf6a8eab68593
hielf <hielf@qq.com>
2017-01-10 22:26:34 +0800

    Revert "migrate session store to active_record"
    This reverts commit 45ab04687f35380d0f755488bfeb74b51803bdab.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,11 +2,10 @@
 module SessionsHelper
 
   def sign_in(user)
-    # cookies.signed[:remember_token] = [user.id, user.salt]
-    # cookies.signed[:secure_user_id] = {secure: true, value: "secure#{user.id}"}
-    cookies.signed[:remember_token] = { :value => [user.id, user.salt],
-                                        :httponly =>      true,
-                                        :cookie_only =>   true }
+    cookies.signed[:remember_token] = [user.id, user.salt]
+    cookies.signed[:secure_user_id] = {secure: true, value: "secure#{user.id}"}
+    # cookies[:remember_token] = { :value => user.id,
+    #                              :expires => 1.day.from_now }
     self.current_user = user
   end
 
@@ -28,7 +27,6 @@ module SessionsHelper
 
   def sign_out
     cookies.delete(:remember_token)
-    reset_session
     self.current_user = nil
   end
 
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,12 +1,12 @@
 # encoding: utf-8
 class Session < ActiveRecord::Base
   attr_accessor :usercode, :password
-  # attr_accessible :login_type, :user_id, :created_at
-  #
-  # belongs_to :user
-  #
-  # scope :online, where("created_at >= ?", 120.minutes.ago)
-  # scope :total_visit, where("created_at >= ?", Date.today)
+  attr_accessible :login_type, :user_id, :created_at
+
+  belongs_to :user
+
+  scope :online, where("created_at >= ?", 120.minutes.ago)
+  scope :total_visit, where("created_at >= ?", Date.today)
 end
 # == Schema Information
 #
@@ -18,3 +18,4 @@ end
 #  created_at :datetime        not null
 #  updated_at :datetime        not null
 #
+
